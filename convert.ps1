55e47472c046495221e5b7cff69d94cc075ee3b4
Thomas Klaner <thomas@iwa-tech.com>
2017-06-09 19:04:16 +0200

    added powershell script

---
+++ convert.ps1
@@ -0,0 +1,25 @@
+
+$inputFolder = "input"
+
+$files = @(Get-ChildItem $inputFolder/*.tif)
+foreach ($file in $files) {
+    $fileName = "$($file.BaseName)$($file.Extension)"
+    Write-Output "convert: $fileName"
+
+    # ToDo: testing only - use lines 12,13 instead of 
+    $command = "echo" 
+    $cmdArgs = Get-Random -input "1","4","8","16","32"
+    # $command = "identify"
+    # $cmdArgs = "-format '%[depth]' $fileName"
+    
+    $bitDepth = & $command $cmdArgs
+    If($bitDepth -eq 1) {
+        Write-Output "run 1-bit convert here"
+        # Todo
+        & "echo" "arguments $fileName" 
+    } else {
+        Write-Output "run convert for all other bit depths here"
+        # Todo
+        & "echo" "arguments $fileName" 
+    }
+}
