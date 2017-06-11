c3a3a5374cd5cecd09fd074debd43c4b39922c5c
Thomas Klaner <thomas@iwa-tech.com>
2017-06-11 21:39:49 +0200

    added new ps which uses docker

---
+++ convert-docker.ps1
@@ -0,0 +1,21 @@
+
+$inputFolder = "input"
+
+$files = @(Get-ChildItem $inputFolder/*.tif)
+foreach ($file in $files) {
+    $fileName = "$($file.BaseName)$($file.Extension)"
+    Write-Output "convert: $fileName"
+
+    $command = "docker"
+    $cmdArgs = 'run --rm -v "$(pwd)/input:/input:ro" -v "$(pwd)/output:/output" kl4n4/tiff-identify-bitdepth $fileName'
+    $bitDepth = & $command $cmdArgs
+    If($bitDepth -eq 1) {
+        Write-Output "run 1-bit convert here"
+        # Todo
+        & "echo" "your-arguments $fileName" 
+    } else {
+        Write-Output "run convert for all other bit depths here"
+        # Todo
+        & "echo" "your-arguments $fileName" 
+    }
+}
--- identify-bitdepth.sh
+++ identify-bitdepth.sh
@@ -3,5 +3,5 @@
 docker run --rm \
     -v "$(pwd)/input:/input:ro" \
     -v "$(pwd)/output:/output" \
-    kl4n4/tiff-converter \
+    kl4n4/tiff-identify-bitdepth \
     $1
